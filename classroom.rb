ae7d6032bca4538dc702a79319a9de243a392167
Tom Daniel <tomudoh258@gmail.com>
2023-06-28 13:58:29 +0100

    Add new files with required relationships

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+require_relative 'students'
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    student.classroom = self
+    @students << student
+  end
+end
