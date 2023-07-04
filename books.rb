e12a1f87dfda301f5c52e2ce2256c3ab1217b46d
Ernest Korir <ernestbore3@gmail.com>
2023-07-04 21:12:03 +0300

    Add to_h method to Person and books

--- books.rb
+++ books.rb
@@ -1,16 +1,21 @@
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
-
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
   end
-
   def add_rental(date, person)
     rental = Rental.new(date, self, person)
     @rentals << rental
     person.add_rental(rental)
+  end
+  def to_h
+    {
+      'type' => 'Book',
+      'title' => @title,
+      'author' => @author
+    }
   end
 end
--- person.rb
+++ person.rb
@@ -1,14 +1,21 @@
 class Person
   attr_reader :id, :age, :parent_permission, :name
-
   def initialize(id, age, parent_permission: true, name: 'Unknown')
     @id = id
     @age = age
     @parent_permission = parent_permission
     @name = name
   end
-
   def can_use_services?
     @age >= 18 || @parent_permission
+  end
+  def to_h
+    {
+      'type' => self.class.to_s,
+      'id' => @id,
+      'age' => @age,
+      'parent_permission' => @parent_permission,
+      'name' => @name
+    }
   end
 end
