be42e04ffb3ebd3a8a652100b3e04a7f6e8d23bd
Ernest Korir <ernestbore3@gmail.com>
2023-07-04 21:14:48 +0300

    Fix Rubocop

--- students.rb
+++ students.rb
@@ -1,12 +1,15 @@
 class Student < Person
   attr_accessor :classroom
+
   def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
     super(id, age, parent_permission: parent_permission, name: name)
     self.classroom = classroom
   end
+
   def play_hooky
     '¯\\_(ツ)_/¯'
   end
+
   def to_h
     {
       'type' => 'Student',
